bcc821eef10e1736f0ab9dfaabca7a49095c1ac8
neffati hajer <116968856+neffati229@users.noreply.github.com>
2023-09-02 05:50:52 +0100

    Add files via upload

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Owner>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+            modelBuilder.Entity<PortfolioItem>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+
+            modelBuilder.Entity<Owner>().HasData(
+                new Owner()
+                {
+                    Id = Guid.NewGuid(),
+                    Avatar = "nouvel_avatar1.jpg",
+                    FullName = "Neffati hajer",
+                    Profil = "Developer"
+                }
+                );
+        }
+
+        public DbSet<Owner> Owner { get; set; }
+        public DbSet<PortfolioItem> PortfolioItems { get; set; }
+    }
+}
